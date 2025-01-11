ce31adcad880712a3043480df17d30dc0364ad09
yun <160323088+zhenyundeng@users.noreply.github.com>
2025-01-11 17:50:11 +0800

    Update __init__.py

--- evaluation_script/__init__.py
+++ evaluation_script/__init__.py
@@ -36,6 +36,8 @@ def install_local_package(folder_name):
 install("scipy")
 install("nltk")
 install("numpy")
+install("aenum")
+install("google-generativeai==0.8.3")
 
 # install("shapely==1.7.1")
 # install("requests==2.25.1")
